8720e5261804396870a890a313cf80eeb32baf49
Joshua Snider <jsnider1938@gmail.com>
2014-08-25 22:29:44 -0700

    Extracting logic to say method

--- calc_app.rb
+++ calc_app.rb
@@ -1,21 +1,35 @@
-puts "What's the first number"
-num = gets.chomp.to_f
+# calc_app.rb
+#
+# a simple calulator app with a bit of user input
+#
+#
 
-puts "What's the operator? ADD, SUBTRACT, MULTIPLY, or DIVIDE?"
+def say(msg)
+  puts "#{msg}"
+end
+
+say "What's the first number"
+num = gets.chomp.to_i
+
+say "What's the operator? ADD, SUBTRACT, MULTIPLY, or DIVIDE?"
 operator = gets.chomp
 
-puts "What's the second number?"
-num_2 = gets.chomp.to_f
+say "What's the second number?"
+num_2 = gets.chomp.to_i
 
 answer = case
 when operator == "ADD"
+  operator = "plus"
   num + num_2
 when operator == "SUBTRACT"
+  operator = "minus"
   num - num_2
 when operator == "MULTIPLY"
+  operator = "times"
   num * num_2
 when operator == "DIVIDE"
+  operator = "divided by"
   num / num_2
 end
 
-puts "The answer is #{answer}"
+say "#{num} #{operator} #{num_2}.  The answer is #{answer}"
