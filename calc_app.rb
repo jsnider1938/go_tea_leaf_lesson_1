ee04f5cdc630005c41edd3edb7758785a586b32e
Joshua Snider <jsnider1938@gmail.com>
2014-08-25 22:42:29 -0700

    practiced keeping raw user input data

--- calc_app.rb
+++ calc_app.rb
@@ -9,27 +9,27 @@ def say(msg)
 end
 
 say "What's the first number"
-num = gets.chomp.to_i
+num = gets.chomp
 
 say "What's the operator? ADD, SUBTRACT, MULTIPLY, or DIVIDE?"
 operator = gets.chomp
 
 say "What's the second number?"
-num_2 = gets.chomp.to_i
+num_2 = gets.chomp
 
 answer = case
 when operator == "ADD"
   operator = "plus"
-  num + num_2
+  num.to_i + num_2.to_i
 when operator == "SUBTRACT"
   operator = "minus"
-  num - num_2
+  num.to_i - num_2.to_i
 when operator == "MULTIPLY"
   operator = "times"
-  num * num_2
+  num.to_i * num_2.to_i
 when operator == "DIVIDE"
   operator = "divided by"
-  num / num_2
+  num.to_f / num_2.to_f
 end
 
 say "#{num} #{operator} #{num_2}.  The answer is #{answer}"
